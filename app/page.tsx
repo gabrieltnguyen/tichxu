9e4db72db9eedf785cffdd3d75121b280c097b10
Gabriel Nguyen <gabrieltnguyen@gmail.com>
2024-08-24 00:14:28 +0700

    update homepages

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,25 @@ export default function Home() {
   return (
     <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
       <Link
-        href="https://github.com/nisabmohd/Documentation-Template"
+        href="https://github.com/gabrieltnguyen/tichxu"
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
       >
-        Follow along on GitHub{" "}
+        Tài liệu được lưu trên Github{" "}
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-7xl">
-        An example app built using Next.js and server components.
+        Mua sắm thả ga, hoàn tiền tối đa!
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-        This feature-packed documentation template, built with Next.js, offers a
-        sleek and responsive design, perfect for all your project documentation
-        needs.
+      Hãy bắt đầu hành trình tiết kiệm thú vị cùng Tích Xu và nhận Hoàn Tiền thật đến 30% khi mua sắm online cộng thêm hàng ngàn ưu đãi, mã giảm giá hấp dẫn từ hàng ngàn cửa hàng như Lazada, Shopee, Tiki,Tiktok Shop, Booking.com, Agoda, Uniqlo, Adidas, Samsung, Apple, Taobao... tại Việt Nam.
       </p>
       <div className="flex flex-row items-center gap-5">
         <Link
           href={`/docs/${page_routes[0].href}`}
           className={buttonVariants({ className: "px-6", size: "lg" })}
         >
-          Get Stared
+          Bắt Đầu Thôi!
         </Link>
         <Link
           href="#"
